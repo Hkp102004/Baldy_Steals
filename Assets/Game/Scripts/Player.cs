baf7ca9240e7ebf41da03cc63e386da4195d303d
Hekey <87144005+Hkp102004@users.noreply.github.com>
2026-09-06 17:25:40 +0530

    feat: used NavMeshAgent to move the Player to where the ray hits the object
    oh it also makes a cube there, so I have to fix that a  lil but it's good for now man

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
@@ -22,6 +23,13 @@ public class Player : MonoBehaviour
             if(Physics.Raycast(rayOrigin, out hitinfo)) // if the ray hits an object
             {
                 Debug.Log(hitinfo.point); // prints the point where the ray hits the object
+
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube); //create a cube;
+
+                cube.transform.position = hitinfo.point; //set the position of the cube to the point where the ray hit the object
+
+                NavMeshAgent agent = GetComponent<NavMeshAgent>();
+                agent.destination = cube.transform.position; //set the destination of the agent to the where the ray hit the object and move the player to that position
             }
         }
     }
